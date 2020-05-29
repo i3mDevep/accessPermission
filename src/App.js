3c0ebed0d0b7f5c85cff5264dec7fade3f6ccb59
DESKTOP-LB6ON16\Michael <mayxool.11@gmail.com>
2020-05-29 15:23:19 -0500

    add route dafult

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { GlobalStyle } from './styles/GlobalStyle'
 import { Dashboard } from './pages/Dashboard'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Context from './Context'
 
@@ -15,10 +15,7 @@ export const App = () => (
         ({isAuth}) => (
             <BrowserRouter>
             <Switch>
-              {/* <Route exact path="/login" component={() => isAuth.loggedIn?
-                <Redirect to="/dashboard" />:
-                <Login />}
-                /> */}
+              <Route exact path="/" component={() => <Login />} />
               <Route exact path="/login" component={() => <Login />} />
               <Route exact path="/register" component={() => <Register />} />
               <Route exact path="/dashboard" component={() => <Dashboard />} />
