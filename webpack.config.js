ffb73764b9bbeb87f2404188b784360b2003e727
Andres Bermúdez <a.arango09@gmail.com>
2020-05-29 22:36:25 -0500

    cambios funcionales

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,57 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
+  entry: './src/index.js',
   output: {
-    filename: 'app.bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        },
       },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
       },
       {
-       test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(s*)css$/,
         use: [
-           'file-loader',
-         ],
-       }
-    ]
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'assets/[hash].[ext]' },
+          },
+        ],
+      },
+    ],
   },
-  devServer: {
-    host: '0.0.0.0',//your ip address
-    port: 8081,
-    disableHostCheck: true
-  }
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+};
