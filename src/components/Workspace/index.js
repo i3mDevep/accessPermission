7fe65c3970f57c242ac10ca56b044ee349fb50a9
DESKTOP-LB6ON16\Michael <mayxool.11@gmail.com>
2020-05-31 17:14:34 -0500

    graficas

--- src/components/Workspace/index.js
+++ src/components/Workspace/index.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import { ContainerWorkspace } from './style'
-import { ListCardsChart } from '../ListCardsChart'
-import { ListCardsInformation } from '../ListCardsInformation'
-import { ListTables } from '../Tables/ListTables'
-import { HeaderPerfil } from '../HeaderPerfil'
+import React from "react";
+import { ContainerWorkspace } from "./style";
+import { ListCardsChart } from "../ListCardsChart";
+import { ListCardsInformation } from "../ListCardsInformation";
+import { ListTables } from "../Tables/ListTables";
+import { HeaderPerfil } from "../HeaderPerfil";
+import './p.css'
+
+import {
+  chartExample1
+} from "../../variables/charts";
+
+import { Card, CardBody } from "reactstrap";
+//data1
+import { Line, Bar } from "react-chartjs-2";
 
 export const Workspace = () => {
-    return(
-      <ContainerWorkspace>
-        <HeaderPerfil />
-        <ListCardsInformation />
-        <ListCardsChart />
-        <ListTables />
-      </ContainerWorkspace>
-    )
-}
+  return (
+    <ContainerWorkspace>
+      <HeaderPerfil />
+      <ListCardsInformation />
+      <Card>
+      <CardBody>
+        <div className="chart-area">
+          <Line data={chartExample1["data1"]} options={chartExample1.options} />
+        </div>
+      </CardBody>
+      </Card>
+      <ListCardsChart />
+      <ListTables />
+    </ContainerWorkspace>
+  );
+};
