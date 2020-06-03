a187c1e92e4fa3e351abbf9935e93a89ccb645dd
Andres Bermúdez <a.arango09@gmail.com>
2020-06-03 17:37:47 -0500

    fix login

--- src/components/UserForm/UserLogin/Login.jsx
+++ src/components/UserForm/UserLogin/Login.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import '../style.scss';
 import {
   Button,
+  Container,
   ButtonGroup,
   Card,
   CardHeader,
@@ -43,39 +44,55 @@ const UserLogin = ({ onSubmit, error, loading }) => {
   };
   return (
     <div className='main-content'>
-      <Form className='m-auto' disabled={loading} onSubmit={handlerSubmit}>
-        <Mylogo
-          style={{ width: '200px' }}
-          src='https://cdn2.iconfinder.com/data/icons/web-store-crayons-volume-2/256/Login-512.png'
-        />
-        <Input
-          disabled={loading}
-          type='email'
-          required={true}
-          placeholder='Email'
-          {...email}
-        />
-        <Input
-          disabled={loading}
-          type='password'
-          required={true}
-          placeholder='Password'
-          {...password}
-        />
-        <Button disabled={loading}>Login</Button>
-        <LoginGoogle>
-          <FaGofore size='30px' />
-          <h1>Login with google</h1>
-        </LoginGoogle>
-        <Rederict disabled={loading}>
-          are you not registered?
-          <Link style={{ color: '#fff' }} to='/register'>
-            sign in
-          </Link>
-        </Rederict>
-        {error && <Error>{error}</Error>}
-        <Foother>developed by ardobot</Foother>
-      </Form>
+      <Col>
+        <Container>
+          <div className='header-body text-center mb-7'>
+            <Row className='justify-content-center'>
+              <Col lg='5' md='6'>
+                <h1 className='text-white'>Bienvenido!</h1>
+                <p className='text-lead text-light'>
+                  Inicie sesión o Cree una cuenta.
+                </p>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+        <Container className="mt--8 pb-5">
+          <Form className='m-auto' disabled={loading} onSubmit={handlerSubmit}>
+            <Mylogo
+              style={{ width: '200px' }}
+              src='https://cdn2.iconfinder.com/data/icons/web-store-crayons-volume-2/256/Login-512.png'
+            />
+            <Input
+              disabled={loading}
+              type='email'
+              required={true}
+              placeholder='Email'
+              {...email}
+            />
+            <Input
+              disabled={loading}
+              type='password'
+              required={true}
+              placeholder='Password'
+              {...password}
+            />
+            <Button disabled={loading}>Login</Button>
+            <LoginGoogle>
+              <FaGofore size='30px' />
+              <h1>Login with google</h1>
+            </LoginGoogle>
+            <Rederict disabled={loading}>
+              are you not registered?
+              <Link style={{ color: '#fff' }} to='/register'>
+                sign in
+              </Link>
+            </Rederict>
+            {error && <Error>{error}</Error>}
+            <Foother>developed by ardobot</Foother>
+          </Form>
+        </Container>
+      </Col>
     </div>
 
   );
