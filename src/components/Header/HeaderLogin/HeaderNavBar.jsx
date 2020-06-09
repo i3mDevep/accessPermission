643c02d8d6c79f11d67b7ece7337f77428cc9689
Andres Bermúdez <a.arango09@gmail.com>
2020-06-08 20:33:30 -0500

    cambios style

--- src/components/Header/HeaderLogin/HeaderNavBar.jsx
+++ src/components/Header/HeaderLogin/HeaderNavBar.jsx
@@ -1,36 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { BsColumnsGap, BsPeopleCircle, BsCompass } from 'react-icons/bs';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { BsTerminal, BsPeopleCircle, BsCompass } from 'react-icons/bs';
+import { Navbar, Nav, Container, Row, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import logo from '../../../assets/img/brand/ardobot_logo.png';
 import './HeaderNavBar.scss';
 
 // reactstrap components
-const navbar = {color: '#ffffff'};
+const navbar = { color: '#ffffff' };
 
 class HeaderNavBar extends React.Component {
   render() {
     return (
       <>
-        <Navbar className='Navbar' varian='dark' expand='lg'>
-          <Navbar.Brand href='dashboard'>
-            <img
-              alt=''
-              src={logo}
-              className='d-inline-block align-top'
-            />
-            {' '}
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Nav.Link href='/dashboard' tag={Link}>Dashboard</Nav.Link>
-              <Nav.Link href='/' tag={Link}>Login</Nav.Link>
-              <Nav.Link href='/register' tag={Link}>Registro</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <div style={{ backgroundColor: '#004876' }}>
+          <div className='container'>
+            <Navbar className='Navbar' variant='dark' expand='lg'>
+              <Navbar.Brand href='dashboard'>
+                <img
+                  alt=''
+                  src={logo}
+                  className='d-inline-block align-top'
+                />
+                {' '}
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls='basic-navbar-nav' />
+              <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className='ml-auto'>
+                  <Nav.Link href='/dashboard' tag={Link}>
+                    <BsTerminal />
+                    {' '}
+                    Dashboard
+                    {' '}
+                  </Nav.Link>
+                  <Nav.Link href='/' tag={Link}>
+                    <BsPeopleCircle />
+                    {' '}
+                    Login
+                    {' '}
+                  </Nav.Link>
+                  <Nav.Link href='/register' tag={Link}>
+                    <BsCompass />
+                    {' '}
+                    Registro
+                    {' '}
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          </div>
+        </div>
       </>
 
     );
--- src/components/UserForm/UserLogin/Login.jsx
+++ src/components/UserForm/UserLogin/Login.jsx
@@ -1,77 +1,36 @@
 import React from 'react';
-import { FaGofore } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { BsFillEnvelopeFill, BsFillShieldLockFill } from 'react-icons/bs';
-import {
-  Button,
-  Card,
-  Container,
-  CardHeader,
-  CardBody,
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Row,
-  Col,
-} from 'reactstrap';
-
-import {
-  Mylogo,
-  Foother,
-  LoginGoogle,
-  Error,
-  Rederict,
-} from './style';
+import { Form, Button } from 'react-bootstrap';
 
 import useInputValue from '../../../hooks/useInputValue';
 
-const UserLogin = ({ onSubmit, error, loading }) => {
-  const email = useInputValue('');
-  const password = useInputValue('');
-  const handlerSubmit = (e) => {
-    e.preventDefault();
-    onSubmit({ email: email.value, password: password.value });
-  };
-  return (
+import './style.scss';
 
-    <Container>
-      <Row>
-        <Col>.col</Col>
-      </Row>
-      <Row>
-        <Col>.col</Col>
-        <Col>.col</Col>
-        <Col>.col</Col>
-        <Col>.col</Col>
-      </Row>
-      <Row>
-        <Col xs='3'>.col-3</Col>
-        <Col xs='auto'>.col-auto - variable width content</Col>
-        <Col xs='3'>.col-3</Col>
-      </Row>
-      <Row>
-        <Col xs='6'>.col-6</Col>
-        <Col xs='6'>.col-6</Col>
-      </Row>
-      <Row>
-        <Col xs='6' sm='4'>.col-6 .col-sm-4</Col>
-        <Col xs='6' sm='4'>.col-6 .col-sm-4</Col>
-        <Col sm='4'>.col-sm-4</Col>
-      </Row>
-      <Row>
-        <Col sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .order-sm-2 .offset-sm-1</Col>
-      </Row>
-      <Row>
-        <Col sm='12' md={{ size: 6, offset: 3 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
-      </Row>
-      <Row>
-        <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-        <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-      </Row>
-    </Container>
+const UserLogin = ({ onSubmit, error, loading }) => {
+
+  return (
+    <div className='main-content'>
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Form.Group controlId='formBasicCheckbox'>
+          <Form.Check type='checkbox' label='Check me out' />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
+    </div>
 
   );
 };
