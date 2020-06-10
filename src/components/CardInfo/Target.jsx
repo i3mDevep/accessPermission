7bcd1f9a10a2d0cb1cd0dfc50ed7a960343d9de1
DESKTOP-LB6ON16\Michael <mayxool.11@gmail.com>
2020-06-10 00:20:46 -0500

    correccion target

--- src/components/CardInfo/Target.jsx
+++ src/components/CardInfo/Target.jsx
@@ -11,35 +11,37 @@ const Target = ({
   typetext,
   children,
 }) => (
-  <Card className='card'>
-    <Card.Body>
-      <Row>
-        <div className='col'>
-          <Card.Title tag='h5' className='text-uppercase text-muted mb-0'>
-            {title || <Skeleton /> }
-          </Card.Title>
-          <span className='h2 font-weight-bold mb-0'>
-            {value || (value !== 0 ? <Skeleton /> : 0)}
-          </span>
-        </div>
-        <Col className='col-1'>
-          <div className='icon icon-shape bg-danger text-white rounded-circle shadow'>
-            <i className='fas fa-chart-bar' />
+  <Col lg='4' className='mb-3'>
+    <Card className='card'>
+      <Card.Body>
+        <Row>
+          <div className='col'>
+            <Card.Title tag='h5' className='text-uppercase text-muted mb-0'>
+              {title || <Skeleton /> }
+            </Card.Title>
+            <span className='h2 font-weight-bold mb-0'>
+              {value || (value !== 0 ? <Skeleton /> : 0)}
+            </span>
           </div>
-        </Col>
-        <Col className='col-3 text-right'>
-          { children || <Skeleton circle={true} height={50} width={50} />}
-        </Col>
-      </Row>
-      <p className='mt-3 mb-0 text-muted text-sm'>
-        <span className={`${typetext || <Skeleton />} mr-2`}>
-          <i className='fa fa-arrow-up' />
-          {porcentage}
-        </span>
-        <span className='text-nowrap'>{text || <Skeleton />}</span>
-      </p>
-    </Card.Body>
-  </Card>
+          <Col className='col-1'>
+            <div className='icon icon-shape bg-danger text-white rounded-circle shadow'>
+              <i className='fas fa-chart-bar' />
+            </div>
+          </Col>
+          <Col className='col-3 text-right'>
+            { children || <Skeleton circle={true} height={50} width={50} />}
+          </Col>
+        </Row>
+        <p className='mt-3 mb-0 text-muted text-sm'>
+          <span className={`${typetext || <Skeleton />} mr-2`}>
+            <i className='fa fa-arrow-up' />
+            {porcentage}
+          </span>
+          <span className='text-nowrap'>{text || <Skeleton />}</span>
+        </p>
+      </Card.Body>
+    </Card>
+  </Col>
 );
 
 export default Target;
