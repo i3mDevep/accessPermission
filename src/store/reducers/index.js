ef9a9f75ad1f0fffeecbce4b2360bbb5c0aebd26
Andres Bermúdez <a.arango09@gmail.com>
2020-06-11 12:17:13 -0500

    cambios 2

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
-import notifications from './notifications';
-
-export default (history) => combineReducers({
-  router: connectRouter(history),
-  notifications,
-});
