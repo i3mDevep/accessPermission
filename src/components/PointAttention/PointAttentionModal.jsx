8aaf2ee0a4175790197d2c035ddad9f99e83cc64
Andres Bermúdez <a.arango09@gmail.com>
2020-06-21 15:35:38 -0500

    add point sale

---
+++ src/components/PointAttention/PointAttentionModal.jsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import { Button, Modal, Form, Col, Alert } from 'react-bootstrap';
+import useInputValue from '../../hooks/useInputValue';
+
+const PointAttentionModal = (props, { onSubmit, loading, error }) => {
+  const namesubcompany = useInputValue('');
+  const email = useInputValue('');
+  const password = useInputValue('');
+  const cellphone = useInputValue('');
+  const address = useInputValue('');
+  const city = useInputValue('');
+  const nameperson = useInputValue('');
+  const estate = useInputValue('');
+  const identification = useInputValue('');
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({
+      email: email.value,
+      password: password.value,
+      namesubcompany: namesubcompany.value,
+      cellphone: cellphone.value,
+      address: address.value,
+      city: city.value,
+      nameperson: nameperson.value,
+      estate: estate.value,
+      identification: identification.value,
+    });
+  };
+
+  return (
+    <Modal
+      {...props}
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Crear Sede o Punto de venta</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form disabled={loading} onSubmit={handlerSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId='formGridEmail'>
+              <Form.Label>Corre Electrónico</Form.Label>
+              <Form.Control
+                type='email'
+                required={true}
+                disabled={loading}
+                {...email}
+                placeholder='Enter email'
+              />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId='formGridPassword'>
+              <Form.Label>Contaseña</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Administre una contraseña'
+                required={true}
+                disabled={loading}
+                {...password}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Group controlId='formGridName'>
+            <Form.Label>Nombre del punto de venta</Form.Label>
+            <Form.Control
+              placeholder='AV / Calle '
+              type='text'
+              required={true}
+              disabled={loading}
+              {...namesubcompany}
+
+            />
+          </Form.Group>
+          <Form.Group controlId='formGridAddress1'>
+            <Form.Label>Dirección</Form.Label>
+            <Form.Control
+              placeholder='AV / Calle '
+              type='text'
+              required={true}
+              disabled={loading}
+              {...address}
+
+            />
+          </Form.Group>
+          <Form.Group controlId='formGridAdmin'>
+            <Form.Label>Persona a Cargo</Form.Label>
+            <Form.Control
+              placeholder=' Administrador de la Tienda'
+              type='text'
+              required={true}
+              disabled={loading}
+              {...nameperson}
+            />
+          </Form.Group>
+
+          <Form.Group controlId='formGridPerson2'>
+            <Form.Label>Teléfono o Celular</Form.Label>
+            <Form.Control
+              placeholder='..'
+              type='text'
+              required={true}
+              disabled={loading}
+              {...cellphone}
+
+            />
+          </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col} controlId='formGridCity'>
+              <Form.Label>Ciudad</Form.Label>
+              <Form.Control
+                disabled={loading}
+                type='text'
+                required={true}
+                {...city}
+
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId='formGridEstate'>
+              <Form.Label>Estado</Form.Label>
+              <Form.Control
+                disabled={loading}
+                type='text'
+                required={true}
+                {...estate}
+              />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId='formGridZip'>
+              <Form.Label>Identificación</Form.Label>
+              <Form.Control
+                disabled={loading}
+                type='text'
+                required={true}
+                {...identification}
+              />
+            </Form.Group>
+          </Form.Row>
+          { error && (
+            <Alert variant='danger'>
+              <small>{error}</small>
+            </Alert>
+          )}
+          <Button
+            disabled={loading}
+            variant='primary'
+            type='submit'
+          >
+            Guardar
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default PointAttentionModal;
--- src/pages/company/CreateSede.jsx
+++ src/pages/company/CreateSede.jsx
@@ -1,125 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as firebase from 'firebase/app';
 import { connect } from 'react-redux';
-import { Card, Container, Row, Button, Modal, Form } from 'react-bootstrap';
+import { Card, Container, CardDeck, Button } from 'react-bootstrap';
 import { IoIosAdd } from 'react-icons/io';
+import PointAttentionModal from '../../components/PointAttention/PointAttentionModal';
+import PointAttentionCard from '../../components/PointAttention/PointAttentionCard';
 
-function MyVerticallyCenteredModal(props) {
-  const handlerOnSubmit = (e) => {
-    e.preventDefault();
-
-  };
-  return (
-    <Modal
-      {...props}
-      size='lg'
-      aria-labelledby='contained-modal-title-vcenter'
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id='contained-modal-title-vcenter'>
-          Crear Nueva Sede
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={handlerOnSubmit}>
-          <Form.Group controlId='formBasicEmail'>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
-            <Form.Text className='text-muted'>
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group controlId='formBasicPassword'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
-          </Form.Group>
-          <Form.Group controlId='formBasicCheckbox'>
-            <Form.Check type='checkbox' label='Check me out' />
-          </Form.Group>
-          <Button variant='primary' type='submit'>
-            Submit
-          </Button>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
 const CreateSede = ({ isAuth }) => {
-  const [modalShow, setModalShow] = React.useState(false);
-  const handlerAddSedes = () => {
+  const [modalShow, setModalShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handlerOnSubmit = ({
+    namesubcompany,
+    email,
+    password,
+    cellphone,
+    address,
+    city,
+    nameperson,
+    estate,
+    identification,
+
+
+  }) => {
     alert('create subcompany');
     const dataSubCompany = {
       company: isAuth.uid,
       content: {
-        name: 'subempresa', email: `emaildesaad@${isAuth.displayName.trim()}.com`, password: 'camilofeo9',
+        namesubcompany,
+        email,
+        password,
+        cellphone,
+        address,
+        city,
+        nameperson,
+        estate,
+        identification,
       },
     };
     const createSubCompany = firebase.functions().httpsCallable('createSubCompany');
     createSubCompany(dataSubCompany)
-      .then((result) => console.log(result))
-      .catch((error) => console.log(error));
+      .then((result) => {
+        setError('');
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.code);
+      });
   };
   return (
-    <Container>
-      <Row>
-        <Card
-          style={{ width: '18rem' }}
-          className='m-2'
-        >
-          <Card.Body>
-            <Card.Title>
-              Crear nueva sede
-            </Card.Title>
 
-            <Button onClick={handlerAddSedes} style={{ padding: '3px' }}><IoIosAdd size='30' /></Button>
-          </Card.Body>
-        </Card>
-        {
-          [
-            'Dark',
-            'Dark',
-            'Dark',
-            'Dark',
-            'Dark',
-            'Dark',
-            'Dark',
-            'Dark',
-          ].map((variant, idx) => (
+    <Container fluid>
+      <Card>
+        <Card.Header>
+          <Button onClick={() => setModalShow(true)} style={{ padding: '3px' }}>
+            <IoIosAdd size='30' />
+            {' '}
+            Crear Nueva Sede
+            {' '}
+          </Button>
+        </Card.Header>
+      </Card>
 
-            <Card
-              bg={variant.toLowerCase()}
-              key={idx}
-              text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-              style={{ width: '18rem' }}
-              className='m-2'
-            >
-              <Card.Header>{`Sede ${1}`}</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  {variant}
-                  Name de la sede
-                </Card.Title>
-                <Card.Text>
-                  Info de la sede
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          ))
-        }
-        <Button variant='primary' onClick={() => setModalShow(true)}>
-          Launch vertically centered modal
-        </Button>
-
-        <MyVerticallyCenteredModal
+      <CardDeck>
+        <PointAttentionCard
+          name='prueba'
+          city='medelin'
+          tlephone='324234'
+        />
+        <PointAttentionCard
+          name='prueba'
+          city='medelin'
+          tlephone='324234'
+        />
+        <PointAttentionModal
+          onSubmit={handlerOnSubmit}
           show={modalShow}
+          error={error}
+          loading={loading}
           onHide={() => setModalShow(false)}
         />
-      </Row>
+      </CardDeck>
     </Container>
   );
 };
