ae7e5e3e32d40002001c04c800787c86e42cb8bc
Andres Bermúdez <a.arango09@gmail.com>
2020-06-23 17:47:45 -0500

    cambios redux

--- src/store/actions/deleteSubcompanyAction.js
+++ src/store/actions/deleteSubcompanyAction.js
@@ -1,14 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import firebase from 'firebase/app';
+import 'firebase/auth';
 
 export const deleteSubcompany = (idBusiness, idSubcompany) => {
   return (dispatch) => {
     const db = firebase.firestore();
-    db.collection('business').doc(idBusiness).collection('subcompanies').doc(idSubcompany)
-      .delete()
-      .then(() => {
-        dispatch({ type: 'DELETE_SUBCOMPANY_SUCCESS' });
-      })
+    firebase.auth().deleteUser(idSubcompany).then((user) => {
+      return db.collection('business').doc(idBusiness).collection('subcompanies').doc(idSubcompany)
+        .delete();
+    }).then(() => {
+      dispatch({ type: 'DELETE_SUBCOMPANY_SUCCESS' });
+
+    })
       .catch((err) => {
         dispatch({ type: 'DELETE_SUBCOMPANY_ERROR' }, err);
       });
