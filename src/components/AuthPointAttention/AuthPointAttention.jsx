834a022720ba33ec7310da00cf184980d7b51b34
Andres Bermúdez <a.arango09@gmail.com>
2020-07-04 13:37:11 -0500

    fix responsive, falta conexión

--- src/components/AuthPointAttention/AuthPointAttention.jsx
+++ src/components/AuthPointAttention/AuthPointAttention.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Webcam from 'react-webcam';
+//import Card from '@material-ui/core/Card';
+import PropTypes from 'prop-types';
+import SweetAlert from 'react-bootstrap-sweetalert';
+import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import { Button, Card, Container, Modal, Form, Row, Col, Alert, CardDeck } from 'react-bootstrap';
+import { showAlert } from '../../store/actions/sweetAlertActions';
+import { getVisibleAlert } from '../../store/reducers/notificationRecucers';
+import useInputValue from '../../hooks/useInputValue';
 
+const AuthPointAttention = ({ init, visibleAlert, worker, isAuth, showAlert, sedes = [] }) => {
 
-const AuthPointAttention = () => {
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
   const [value, setValue] = useState(new Date());
+  const [Sede, setSede] = useState({ value: '', id: '' });
 
   useEffect(() => {
     const interval = setInterval(
@@ -29,6 +36,13 @@ const AuthPointAttention = () => {
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
+    showAlert({
+      type: 'success',
+      title: 'Good!',
+      content: 'Registro Exitoso',
+      timeout: 3000,
+      showCancel: false,
+    });
   }, [webcamRef, setImgSrc]);
 
   const useStyles = makeStyles({
@@ -41,41 +55,127 @@ const AuthPointAttention = () => {
   });
   const classes = useStyles();
 
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
   return (
-    <>
-      <Card className={classes.root}>
-        <CardContent>
-          <Webcam
-            width='600'
-            height='600'
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat='image/jpeg'
-          />
-          <CardActions>
-            <Button className='primary' onClick={capture}>Capture photo</Button>
-          </CardActions>
+
+    <Container fluid>
+      <Row>
+        {visibleAlert && <SweetAlert {...visibleAlert}>{visibleAlert.content}</SweetAlert>}
+        <Col sm={10} md={6} style={{ margin: '0 auto' }}>
+          <Card>
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='h2'>
+                Hora de Registro
+              </Typography>
+              <Webcam
+                className='embed-responsive embed-responsive-16by9'
+                audio={false}
+                ref={webcamRef}
+                screenshotFormat='image/jpeg'
+                text='img'
+                controls
+              />
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {h % 12}
+                :
+                {(m < 10 ? `0${m}` : m)}
+                :
+                {(s < 10 ? `0${s}` : s)}
+                {' '}
+                {h < 12 ? 'am' : 'pm'}
+              </Typography>
+              <br />
+              <Button className='primary' onClick={capture}>Registrar</Button>
+            </CardContent>
+          </Card>
+          <br />
+        </Col>
+        <br />
+        <Col sm={10} md={6} style={{ margin: '0 auto' }}>
+          <Card>
+            <CardContent>
+              <Form>
+                <Form.Group controlId='Identification'>
+                  <Form.Label>Seleccione su documento</Form.Label>
+                  <Form.Control
+                    as='select'
+                    value={Sede.value}
+                    onChange={(e) => {
+                      const index = e.target.selectedIndex;
+                      setSede({ value: e.target.value, id: e.target.options[index].id });
+                    }}
+                  >
+                    <option>Seleccione una sede o Lugar de trabajo</option>
+                    {
+                      sedes.map((subCompany) => <option id={subCompany.id} key={`id-${subCompany.id}`}>{subCompany.namesubcompany}</option>)
+                    }
+                  </Form.Control>
+                </Form.Group>
+              </Form>
+            </CardContent>
+          </Card>
+          <br />
+          <Card>
+            <CardContent>
+              <Form>
+                <Form.Group controlId='Name'>
+                  <Form.Label>Nombre</Form.Label>
+                  <Form.Control
+                    placeholder='Nombre'
+                    required
+                    disabled={true}
+                  />
+                </Form.Group>
+                <Form.Group controlId='LastName'>
+                  <Form.Label>Apellido</Form.Label>
+                  <Form.Control
+                    placeholder='Apellido'
+                    required
+                    disabled={true}
+                  />
+                </Form.Group>
+              </Form>
+            </CardContent>
+          </Card>
+          <br />
+
           {imgSrc && (
-            <img
-              alt='webcam'
-              text='name'
-              className={classes.media}
-              src={imgSrc}
-            />
+            <Card className='text-center'>
+              <Card.Body>
+                <img
+                  alt='webcam'
+                  text='name'
+                  className={classes.media}
+                  src={imgSrc}
+                />
+              </Card.Body>
+            </Card>
           )}
-          <h3>
-            {h % 12}
-            :
-            {(m < 10 ? `0${m}` : m)}
-            :
-            {(s < 10 ? `0${s}` : s)}
-            {' '}
-            {h < 12 ? 'am' : 'pm'}
-          </h3>
-        </CardContent>
-      </Card>
-    </>
+
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default AuthPointAttention;
+AuthPointAttention.propTypes = {
+  visibleAlert: PropTypes.any,
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    showAlert: (alertProps) => dispatch(showAlert(alertProps)),
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.auth.isAuth,
+    visibleAlert: getVisibleAlert(state.notifications),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthPointAttention);
