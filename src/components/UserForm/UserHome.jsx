d42d74b14e1fe9ee7fcc5b20931dafcc0b691482
DESKTOP-LB6ON16\Michael <mayxool.11@gmail.com>
2020-07-31 19:30:22 -0500

    estilando

--- src/components/UserForm/UserHome.jsx
+++ src/components/UserForm/UserHome.jsx
@@ -17,34 +17,20 @@ import { FaReact } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 import { Background } from './style';
 import HeaderNavBar from '../LayoutUserForm/Header';
-
-function Copyright() {
-  return (
-    <Typography variant='body2' color='textSecondary' align='center'>
-      {'Copyright © '}
-      <Link color='inherit' href='https://www.ardobot.com/'>
-        Ardobot Robótica SAS
-      </Link>
-      {' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import './userhome.scss';
 
 const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(2),
   },
   heroContent: {
-    backgroundColor: '#f7f6f4',
-    padding: theme.spacing(8, 0, 6),
+    backgroundColor: '#1b6ca8',
+    padding: theme.spacing(4, 0, 6),
   },
   heroButtons: {
     marginTop: theme.spacing(4),
   },
   cardGrid: {
-    paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
   card: {
@@ -78,84 +64,56 @@ const UserHome = () => {
         {/* Hero unit */}
         <Row className={classes.heroContent}>
           <Col sm='12' md='5'>
-            <img className='w-100' alt='port' src='https://img.freepik.com/free-vector/qr-code-scanning_41910-348.jpg?size=626&ext=jpg' />
+            <img className='w-75' style={{ maxWidth: '500px', position: 'absolute' }} alt='port' src='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2F5.png?alt=media&token=520496b5-3dfc-43f7-972c-41b1015de39d' />
           </Col>
           <Col sm='12' md='6'>
-            <Container className='p-5'>
+            <Container className='p-5 container_user_home'>
               <h1 className='text-center p-2' style={{ fontSize: '1.8rem' }}>
                 ¿En que consiste?
               </h1>
-              <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              <Typography variant='h5' style={{ fontSize: '1.2rem', padding: '20px' }} align='center' paragraph>
                 Somos una plataforma tecnológica, de uso gratuito  que  te permite
                 cumplir con el control de bioseguridad para tu negocio, reportes en
                 Tiempo Real, y registro de temperatura de tus clientes y trabajadores
               </Typography>
-              <Typography variant='caption' align='center' color='textSecondary' paragraph>
+              <Typography variant='caption' align='center' paragraph>
                 *Aplican Terminos y Condiciones
               </Typography>
             </Container>
           </Col>
         </Row>
-
-        <Container className={classes.cardGrid} maxWidth='md'>
-          <Grid container justify='center'>
-            <Grid item>
-              <Tooltip title='Add'>
-                <Typography component='h1' variant='h3' align='center' color='textPrimary' gutterBottom>
-                  Características
-                </Typography>
-              </Tooltip>
-            </Grid>
-          </Grid>
-          <Grid container justify='center'>
-            <Grid item xs={6}>
-              <Tooltip title='Add' placement='left-start'>
-                <SystemUpdateIcon background='blue' />
-              </Tooltip>
-              <br />
-              <Tooltip title='Add' placement='left'>
-                <Button>left</Button>
-              </Tooltip>
-              <br />
-              <Tooltip title='Add' placement='left-end'>
-                <Button>left-end</Button>
-              </Tooltip>
-            </Grid>
+        <Row>
+          <Col style={{ display: 'flex', justifyContent: 'center', alignContent:'center', padding:'50px' }}>
             <Grid item container xs={6} alignItems='flex-end' direction='column'>
               <Grid item>
-                <Tooltip title='Add' placement='right-start'>
-                  <Button>right-start</Button>
-                </Tooltip>
+                <CircleImage title='Escaner de cedula' URL='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2Finformes.png?alt=media&token=018395b1-95e5-41d9-aa81-3f5ed0966aa2' />
               </Grid>
               <Grid item>
-                <Tooltip title='Add' placement='right'>
-                  <Button>right</Button>
-                </Tooltip>
+                <CircleImage title='Escaner de cedula' URL='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2Flocalizacion.png?alt=media&token=3609f498-2843-4d88-be2c-dbfd21d9db82' />
               </Grid>
               <Grid item>
-                <Tooltip title='Add' placement='right-end'>
-                  <Button>right-end</Button>
-                </Tooltip>
+                <CircleImage title='Escaner de cedula' URL='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2Fregistro.png?alt=media&token=585004ab-b58f-42a2-a75d-9058bcca2094' />
               </Grid>
             </Grid>
-          </Grid>
-          <Grid container justify='center'>
-            <Grid item>
-              <Tooltip title='Add' placement='bottom-start'>
-                <Button>bottom-start</Button>
-              </Tooltip>
-              <Tooltip title='Add' placement='bottom'>
-                <Button>bottom</Button>
-              </Tooltip>
-              <Tooltip title='Add' placement='bottom-end'>
-                <Button>bottom-end</Button>
-              </Tooltip>
+          </Col>
+          <Col style={{ display: 'flex', justifyContent: 'center', alignContent:'center', padding:'50px' }}>
+            <Grid item xs={6} alignItems='flex-end' direction='row'>
+              <Grid item>
+                <CircleImage title='Escaner de cedula' URL='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2Fregistro.png?alt=media&token=585004ab-b58f-42a2-a75d-9058bcca2094' />
+              </Grid>
+              <Grid item>
+                <CircleImage title='Escaner de cedula' URL='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2Fcontrol%20de%20fechas.png?alt=media&token=76896c86-09ee-4941-8ee0-be83760aaac8' />
+              </Grid>
+              <Grid item>
+                <CircleImage title='Escaner de cedula' URL='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2Fdescarga.png?alt=media&token=58d884c6-502e-45ae-a908-c8a58ee0c93a' />
+              </Grid>
             </Grid>
-          </Grid>
-
-        </Container>
-        <Container className={classes.cardGrid} maxWidth='md'>
-          {/* End hero unit */}
+          </Col>
+          <Col style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <img alt='sdas' style={{ width: '75%', minWidth:'400px', objectFit:'cover' }} src='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2F3.png?alt=media&token=532e76cc-70e2-4d1a-be4f-c3eeb3dae3a9' />
+          </Col>
+        </Row>
+        {/* <Container className={classes.cardGrid} maxWidth='md'>
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
@@ -185,31 +143,40 @@ const UserHome = () => {
               </Grid>
             ))}
           </Grid>
-        </Container>
+        </Container> */}
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
         <Row>
           <Col style={{ textAlign: 'center' }}>
             <h4 style={{ fontSize: '1.4rem', color: 'white' }}>Powered by</h4>
-            <br/>
+            <br />
             <FaReact size={100} color='white' />
           </Col>
           <Col style={{ textAlign: 'center' }}>
             <h4 style={{ fontSize: '1.4rem', color: 'white' }}>Developed with</h4>
-            <br/>
+            <br />
             <img alt='logo' src='https://firebasestorage.googleapis.com/v0/b/coronavirus-control.appspot.com/o/recourses%2Fardobot_logo.png?alt=media&token=6249da59-eaa5-4ea6-a655-9c4932d11b7c' />
           </Col>
           <Col style={{ textAlign: 'center' }}>
             <h4 style={{ fontSize: '1.4rem', color: 'white' }}>Support in</h4>
-            <br/>
-            <img style={{height:'100px'}} alt='firebase' src='https://firebase.google.com/downloads/brand-guidelines/PNG/logo-vertical.png?hl=es-419'/>
+            <br />
+            <img style={{ height: '100px' }} alt='firebase' src='https://firebase.google.com/downloads/brand-guidelines/PNG/logo-vertical.png?hl=es-419' />
           </Col>
         </Row>
       </footer>
       {/* End footer */}
-      );
     </>
+  );
+};
+
+const CircleImage = ({ URL, title }) => {
+  return (
+    <Tooltip title={title} placement='left-end'>
+      <Button style={{ borderRadius: '100%' }}>
+        <img style={{ width: '100px' }} alt='imagecircle' src={URL} />
+      </Button>
+    </Tooltip>
   );
 };
 export default UserHome;
